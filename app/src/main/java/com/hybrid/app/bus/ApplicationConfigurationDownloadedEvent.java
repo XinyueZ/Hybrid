9e16d0008be7a3632a8f9083ae25ed118cff8a78
Xinyue Zhao <czhao@cellular.de>
2014-07-23 15:11:04 +0200

    Removed old bootstrap that hard coding in app, prepare for lib.

--- app/src/main/java/com/hybrid/app/bus/ApplicationConfigurationDownloadedEvent.java
+++
@@ -1,7 +0,0 @@
-package com.hybrid.app.bus;
-
-/**
- * Event sent after application's configuration has been loaded and read completely.
- */
-public final class ApplicationConfigurationDownloadedEvent {
-}
